931e46d9a57a419f6c78a822034a9f80a3dc7cfc
Ko Takagi <knockoutmarch@gmail.com>
2016-12-04 15:50:03 +0900

    将棋連盟サイトリニューアルに伴って修正

--- extension/shinya.js
+++ extension/shinya.js
@@ -1,7 +1,16 @@
 window.addEventListener("load", function() {
-    var el = document.getElementsByClassName("kishi")[0];
+    console.debug("load");
+    var el = document.getElementsByClassName("imgArea")[0];
     el.addEventListener("click", function() {
         var img = this.getElementsByTagName("img")[0];
-        img.src = "http://www.shogi.or.jp/topics/photo/satou-sin_01.jpg";
+        var dataset = img.dataset;
+        if (dataset.src == undefined) {
+            dataset.src = img.src;
+            img.src = "http://www.shogi.or.jp/news/entry_images/satou-sin_01.jpg";
+        } else {
+            var _src = dataset.src;
+            dataset.src = img.src;
+            img.src = _src;
+        }
     }); 
 }, false);
